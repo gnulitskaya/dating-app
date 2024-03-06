146cbc54265c7d5dc9ba86dede3722572e40d98a
gnulitskaya <webgnule@mail.ru>
2024-03-06 22:27:41 +0300

    add methods

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { catchError, tap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'client';
+export class AppComponent implements OnInit {
+  title = 'App Dating';
+  users: any;
+
+  constructor(private http: HttpClient) {
+
+  }
+
+  ngOnInit(): void {
+    this.getUsers();
+  }
+
+  getUsers() {
+    this.http.get('http://localhost:5000/api/users')
+    .pipe(
+      tap(users => {
+        this.users = users;
+        console.log('users', users);
+      }),
+      catchError((err: any) => {
+        console.log('err', err);
+        return err;
+      })
+    )
+    .subscribe()
+  }
 }
