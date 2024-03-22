7b031e52f09061c5e843aab000f194333c9df693
gnulitskaya <webgnule@mail.ru>
2024-03-22 14:34:57 +0300

    add repository

---
+++ API/Data/UserRepository.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using API.Entities;
+using API.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Data
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly DataContext _context;
+    
+        public UserRepository(DataContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<AppUser>> GetAppUsers()
+        {
+            return await _context.Users.ToListAsync();
+        }
+
+        public async Task<AppUser> GetUserByIdAsync(int id)
+        {
+            return await _context.Users.FindAsync(id);
+        }
+
+        public async Task<AppUser> GetUserByUsernameAsync(string username)
+        {
+            return await _context.Users.SingleOrDefaultAsync(x => x.UserName == username);
+        }
+
+        public async Task<bool> SaveAllAsync()
+        {
+            return await _context.SaveChangesAsync() > 0;
+        }
+
+        public void Update(AppUser user)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
